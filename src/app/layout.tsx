a622c4f267eb793198d6a070b2f62b425c90c95a
Imad <imadocrate@gmail.com>
2026-06-18 18:53:44 +0100

    fix: hardcode google analytics ID to ensure it runs in production

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -141,22 +141,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         </PageWrapper>
         <Footer />
         <Analytics />
-        {process.env.NEXT_PUBLIC_GA_ID && (
-          <>
-            <Script
-              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-              strategy="afterInteractive"
-            />
-            <Script id="google-analytics" strategy="afterInteractive">
-              {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
-              `}
-            </Script>
-          </>
-        )}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-F0FWMJXBPS"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-F0FWMJXBPS');
+          `}
+        </Script>
         {/* Social Bar Script */}
         <script type="text/javascript" src="https://pl29770203.effectivecpmnetwork.com/34/94/04/349404a6f693ba2d8907d7b787e9513e.js"></script>
       </body>
